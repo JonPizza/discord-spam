4c8fc1354aefea89d29c7eac121ee13cdaf4596c
JonPizza <45501389+JonPizza@users.noreply.github.com>
2019-03-25 20:50:08 -0400

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,1 +1,40 @@
+from random import randint
 
+class UserJonPizza:
+  def __init__(self, rage=True):
+    rage = self.rage
+    
+    while rage == True:
+      unrage = randint(0, 30)
+      print('UserJonPizza is raging\n')
+      if unrage == 21:
+        self.rage = False
+        
+  def moar_rage(self, date):
+    print(f'Benny is Gay, and is dating {date}\n')
+    
+  def get_high(self):
+    print('I already am\n')
+    
+  def buy_drugs(self):
+    print("I can't I spent it all on Drugs and Ravencoin\n")
+    
+  class seal_thingy:
+    def __init__(self):
+      for i in range(30):
+        print('i, seal thingy, am gay')
+        
+    def is_gay(self):
+      print('Fun fact: If you translate "Fish 2 Give" from english to chinese and say it, it sounds like "You are Gay"')
+      
+    class benny:
+      def __init__(self, gay=True):
+        gay = self.gay
+        
+        if gay == False:
+          print('Whoops! Did i let you not be gay? Changing to true form...')
+          gay = True
+          
+      def dies(self):
+        print('Benny dies. This is a good day')
+      
